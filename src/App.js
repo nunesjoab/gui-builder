ba3afc39696af0b516c4906a41c1c3e9740c64f2
joab-cpqd <joab@cpqd.com.br>
2019-08-23 16:52:30 -0300

    Setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DojotButton from './components/DojotButton/DojotButton';
 
 function App() {
   return (
-      <div className="App">
-          <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                 Edit
-                  {' '}
-                  <code>src/App.js</code>
-                  {' '}
-                and save to reload.
-              </p>
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
-              </a>
-          </header>
-      </div>
+        </a>
+        <DojotButton
+          alt=""
+          color="primary"
+          icon=""
+          label="Novo Botão"
+          link="javascript:void(0)"
+          shape="default"
+          size="small"
+        />
+      </header>
+    </div>
   );
 }
 
